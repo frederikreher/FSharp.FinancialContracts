30afa67e28ea113ddebdd011057f1f9c99710f75
Rasr <rasr@itu.dk>
2018-02-16 11:41:07 +0100

    started work on simple implementation of the Contract types

---
+++ FSharp.FinancialContracts/Contract.fs
@@ -0,0 +1,31 @@
+﻿namespace FSharp.FinancialContracts
+
+open System
+
+module Contract =
+
+    type Currency = GBP | USD | DKK
+
+    // Defines how a contract can be constructed
+    type Contract = 
+      | Trade of int * Currency
+      | After of DateTime * Contract
+      | Until of DateTime * Contract
+      | Combine of Contract * Contract
+
+    // Evaluate contract on a specific day
+    let rec eval contract (day:DateTime) = 
+      [ match contract with
+        | Trade(a, n) -> yield (a, n) 
+        | Combine(c1, c2) -> 
+            yield! eval c1 day
+            yield! eval c2 day
+        | After(dt, c) when day > dt -> yield! eval c day
+        | Until(dt, c) when day < dt -> yield! eval c day
+        | _ -> () ]
+
+    // Functions for creating contracts
+    let trade (amount, what) = Trade (amount, what)
+    let after dt contract = After (dt, contract)
+    let until dt contract = Until (dt, contract)
+    let ($) c1 c2 = Combine(c1, c2)
---
+++ FSharp.FinancialContracts/Contract.fsi
@@ -0,0 +1,15 @@
+﻿namespace FSharp.FinancialContracts
+
+open System
+
+module Contract =
+    
+    type Currency
+    type Contract
+
+    val eval: Contract -> (int * Currency) list
+
+    val trade : int -> Currency -> Contract
+    val after : DateTime -> Contract -> Contract
+    val until : DateTime -> Contract -> Contract
+    val ($) : Contract -> Contract -> Contract
