7dc4ac2d5e84fffaf8a92fe1d47ac13a23cdb3d6
Rasr <rasr@itu.dk>
2018-02-19 15:05:57 +0100

    updated the .fsi file and implemented some of the functionality

--- FSharp.FinancialContracts/Contract.fs
+++ FSharp.FinancialContracts/Contract.fs
@@ -4,28 +4,60 @@ open System
 
 module Contract =
 
+    // Currency used in contracts
     type Currency = GBP | USD | DKK
+
+    // Time difference
+    type Days = Double
 
     // Defines how a contract can be constructed
     type Contract = 
       | Trade of int * Currency
       | After of DateTime * Contract
       | Until of DateTime * Contract
-      | Combine of Contract * Contract
-
-    // Evaluate contract on a specific day
-    let rec eval contract (day:DateTime) = 
-      [ match contract with
-        | Trade(a, n) -> yield (a, n) 
-        | Combine(c1, c2) -> 
-            yield! eval c1 day
-            yield! eval c2 day
-        | After(dt, c) when day > dt -> yield! eval c day
-        | Until(dt, c) when day < dt -> yield! eval c day
-        | _ -> () ]
+      //| Zero of Contract
+      | One of Currency
+      | Give of Contract
+      | And of Contract * Contract
+      | Or of Contract * Contract
+      | Truncate of DateTime * Contract
+      | Then of Contract * Contract
+      | Scale of Double * Contract
+      | Get of Contract
+      | Anytime of Contract
+      
+    let date s = DateTime.Parse s
+    let diff (d1:DateTime) (d2:DateTime) : Days = (d1.Subtract d2).TotalDays
+    let add (d1:DateTime) (days:Days) = d1.AddDays days
 
     // Functions for creating contracts
     let trade (amount, what) = Trade (amount, what)
     let after dt contract = After (dt, contract)
     let until dt contract = Until (dt, contract)
-    let ($) c1 c2 = Combine(c1, c2)
+
+    //let zero
+    let one currency = One(currency)
+    let give c = Give(c)
+    let _and_ c1 c2 = And(c1, c2)
+    let _or_ c1 c2 = Or(c1, c2)
+    let truncate d1 c1 = Truncate(d1, c1)
+    let _then_ c1 c2 = Then(c1, c2)
+    let scale d c1 = Scale(d, c1)
+    let get c = Get(c)
+    let anytime c = Anytime(c)
+
+    // Evaluate contract on a specific day
+    let rec eval contract (day:DateTime) = 
+      [ match contract with
+        | Trade(a, n) -> yield (a, n) 
+        | After(dt, c) when day > dt -> yield! eval c day
+        | Until(dt, c) when day < dt -> yield! eval c day
+        | One(currency) -> yield (1, currency)
+        | Give(c) -> 
+            match eval c day with
+            | [(a, n)] -> yield (-a, n)
+            | _ -> failwith "Could not evaluate the 'Give' contract"
+        | And(c1, c2) -> 
+            yield! eval c1 day
+            yield! eval c2 day
+        | _ -> () ]
--- FSharp.FinancialContracts/Contract.fsi
+++ FSharp.FinancialContracts/Contract.fsi
@@ -5,11 +5,28 @@ open System
 module Contract =
     
     type Currency
+    type Days
     type Contract
 
     val eval: Contract -> (int * Currency) list
 
+    val date : String -> DateTime
+    val diff : DateTime -> DateTime -> Days
+    val add: DateTime -> Days -> DateTime
+
     val trade : int -> Currency -> Contract
     val after : DateTime -> Contract -> Contract
     val until : DateTime -> Contract -> Contract
-    val ($) : Contract -> Contract -> Contract
+
+    // Primitives for defining contracts - See Composing contracts
+    val zero : Contract
+    val one : Currency -> Contract
+    val give : Contract -> Contract
+    val _and_ : Contract -> Contract -> Contract
+    val _or_ : Contract -> Contract -> Contract
+    val truncate : DateTime -> Contract -> Contract
+    val _then_ : Contract -> Contract -> Contract
+    val scale : Double -> Contract -> Contract
+    val get : Contract -> Contract
+    val anytime : Contract -> Contract  
+
