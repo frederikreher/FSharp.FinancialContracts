d1919c88b10df33435cb96288972653e28c6c488
Rasr <rasr@itu.dk>
2018-02-20 09:22:59 +0100

    finished rudimentary implementation of financial contracts in fsharp

--- FSharp.FinancialContracts/Contract.fs
+++ FSharp.FinancialContracts/Contract.fs
@@ -5,26 +5,31 @@ open System
 module Contract =
 
     // Currency used in contracts
-    type Currency = GBP | USD | DKK
+    type Currency = GBP | USD | DKK | None
 
     // Time difference
     type Days = Double
 
     // Defines how a contract can be constructed
     type Contract = 
-      | Trade of int * Currency
-      | After of DateTime * Contract
-      | Until of DateTime * Contract
-      //| Zero of Contract
-      | One of Currency
-      | Give of Contract
-      | And of Contract * Contract
-      | Or of Contract * Contract
-      | Truncate of DateTime * Contract
-      | Then of Contract * Contract
-      | Scale of Double * Contract
-      | Get of Contract
-      | Anytime of Contract
+      | Trade of Double * Currency          // Standard contract with an infinite horizon.
+      | After of DateTime * Contract        // Standard contract with an infinite horizon from the provided date.
+      | Until of DateTime * Contract        // Standard ccontract with finite horizon.
+      | Zero of Double * Currency           // Contract that has no rights or obligations and has an infinite horizon.
+      | One of Currency                     // Contract paying one unit of the provided currency. Has an infinite horizon.
+      | Give of Contract                    // Contract giving away the provided contract. 
+                                            // E.g. X acquiring c from Y, implies that Y 'give c'.
+      | And of Contract * Contract          // Immediately acquire both contracts.
+      | Or of Contract * Contract           // Immediately acquire either of the contracts.
+      | Truncate of DateTime * Contract     // Contract that expires at the provided time, 
+                                            // but have the horizion of the provided contract.
+      | Then of Contract * Contract         // Acquire the first of the provided contracts unless it has expired
+                                            // in which case the second contract is acquired.
+      | Scale of Double * Contract          // Acquire the provided contract, but all rights and obligations 
+                                            // is scaled by the provided value.
+      | Get of Contract                     // Acquire the provided contract on its expiration date.
+      | Anytime of Contract                 // Acquire the provided contract at anytime between the acquisition 
+                                            // date of this contract and the expiration date of the provided contract.
       
     let date s = DateTime.Parse s
     let diff (d1:DateTime) (d2:DateTime) : Days = (d1.Subtract d2).TotalDays
@@ -35,7 +40,7 @@ module Contract =
     let after dt contract = After (dt, contract)
     let until dt contract = Until (dt, contract)
 
-    //let zero
+    let zero = Zero(0.0, None)
     let one currency = One(currency)
     let give c = Give(c)
     let _and_ c1 c2 = And(c1, c2)
@@ -48,16 +53,41 @@ module Contract =
 
     // Evaluate contract on a specific day
     let rec eval contract (day:DateTime) = 
-      [ match contract with
-        | Trade(a, n) -> yield (a, n) 
-        | After(dt, c) when day > dt -> yield! eval c day
-        | Until(dt, c) when day < dt -> yield! eval c day
-        | One(currency) -> yield (1, currency)
-        | Give(c) -> 
-            match eval c day with
-            | [(a, n)] -> yield (-a, n)
-            | _ -> failwith "Could not evaluate the 'Give' contract"
-        | And(c1, c2) -> 
-            yield! eval c1 day
-            yield! eval c2 day
-        | _ -> () ]
+        [ match contract with
+            | Trade(a, n) -> yield (a, n) 
+            | After(dt, c) when day > dt -> yield! eval c day
+            | Until(dt, c) when day < dt -> yield! eval c day
+            | Zero(a, n) -> yield (a, n)
+            | One(currency) -> yield (1.0, currency)
+            | Give(c) -> 
+                match eval c day with
+                | [(a, n)] -> yield (-a, n)
+                | _ -> failwith "Could not evaluate the 'Give' contract"
+            | And(c1, c2) -> 
+                yield! eval c1 day
+                yield! eval c2 day
+            | Or(c1, c2) ->
+                let rand = new Random()
+                if (rand.Next(0,2) = 1) then 
+                    yield! eval c1 day
+                else
+                    yield! eval c2 day
+            | Truncate(d1, c1) when day < d1 -> yield! eval c1 day
+            | Then(c1, c2) -> 
+                match eval c1 day with
+                | [(a, n)] -> yield (a, n)
+                | [] -> yield! eval c2 day
+                | _ -> failwith "Could not evaluate the 'Then' contract"
+            | Scale(d, c1) ->
+                match eval c1 day with
+                | [(a, n)] -> yield (d * a, n)
+                | _ -> failwith "Could not evaluate the 'Scale' contract"
+            | Get(c) ->
+                match c with
+                | Until(dt, c2) when dt = day -> yield! eval c2 day
+                | _ -> failwith "Could not evaluate the 'Get' contract. 'Get' has to contain a 'Until' contract"
+            | Anytime(c) ->
+                match c with
+                | Until(dt, c2) -> yield! eval c day
+                | _ -> failwith "Could not evaluate the 'Anytime' contract. 'Anytime' has to contain a 'Until' contract"
+            | _ -> () ]
--- FSharp.FinancialContracts/Program.fs
+++ FSharp.FinancialContracts/Program.fs
@@ -1,8 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.org
+namespace FSharp.FinancialContracts
 
 open System
+open Contract
 
-[<EntryPoint>]
-let main argv =
-    printfn "Give me money"
-    0 // return an integer exit code
+module program = 
+
+    [<EntryPoint>]
+    let main argv =
+        printfn "Give me money"
+        //eval (_and_ (after (date "20-02-2018") test) test2) (date "20-02-2018")
+        0 // return an integer exit code
