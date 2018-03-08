4300df591eca761da8f4e39b27b2485a5386e1ca
Frederik Nikolaj Schaarup Reher <frederikreher@Frederiks-MacBook-Pro.local>
2018-03-08 10:42:22 +0100

    move contracts to seperate file

---
+++ FSharp.FinancialContracts/Contracts.fs
@@ -0,0 +1,12 @@
+﻿namespace FSharp.FinancialContracts
+
+open System
+open Contract
+open Environment
+
+module Contracts =
+
+    // Zero-coupon discount bond
+    let zcb time obs cur = Delay(time, Scale(obs, One(cur)))
+    let zcbF time value cur = Delay(time, Scale(Const value, One(cur)))
+    
