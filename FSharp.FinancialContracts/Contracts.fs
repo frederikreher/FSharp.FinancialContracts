eeadb8158353420a488a8d130103d3c87591dcab
Rasmus Rynning Rasmussen <r.r.rasmussen@hotmail.com>
2018-03-16 11:52:34 +0100

    simplified example contracts

--- FSharp.FinancialContracts/Contracts.fs
+++ FSharp.FinancialContracts/Contracts.fs
@@ -7,16 +7,10 @@ open Contract
 module Contracts =
 
     // Zero-coupon discount bond using an observable.
-    let zcbO time obs asset = Delay(time, Scale(obs, One(asset)))
-    // Zero-coupon discount bond using an constant.
-    let zcbC time value asset = Delay(time, Scale(Const(value), One(asset)))
+    let zcb time obs asset = Delay(time, Scale(obs, One(asset)))
     
     // American option using an observable.
-    let americanO expr time obs asset : Contract = If(expr, time, Scale(obs, One(asset)), Zero)
-    // American option using an constant.
-    let americanC expr time value asset : Contract = If(expr, time, Scale(Const(value), One(asset)), Zero)
+    let american expr time obs asset : Contract = If(expr, time, Scale(obs, One(asset)), Zero)
     
     // European option using an observable.
-    let europeanO expr time obs asset : Contract = Delay(time, If(expr, 0, Scale(obs, One(asset)), Zero))
-    // European option using an constant.
-    let europeanC expr time value asset : Contract = Delay(time, If(expr, 0, Scale(Const(value), One(asset)), Zero))
+    let european expr time obs asset : Contract = Delay(time, If(expr, 0, Scale(obs, One(asset)), Zero))
