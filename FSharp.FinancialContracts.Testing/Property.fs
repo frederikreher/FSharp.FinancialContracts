d949dfe35c23af98328daf0c9541bc47631e7064
Frederik Nikolaj Schaarup Reher <frederikreher@frederikreher.macbook>
2018-03-20 10:55:44 +0100

    worked on tests

--- FSharp.FinancialContracts.Testing/Property.fs
+++ FSharp.FinancialContracts.Testing/Property.fs
@@ -5,27 +5,41 @@ open FSharp.FinancialContracts.Contract
 open FSharp.FinancialContracts.Environment
 
 module Property =
-    type TransactionProperty = (Environment -> Transaction list[] -> bool)
+    type Property = (Environment -> Transaction list -> bool)
+
+
     type BinOp = (float -> float -> bool)
     type Filter = (Transaction -> bool)
 
     let sumByFilter filter = fun (Transaction(f,c)) -> if filter (Transaction(f,c)) then f else 0.0
     let s = op_Equality
-    type Property = 
-        | Predicate of (Environment -> Transaction list[] -> bool)
-        | And of Property * Property
-        | Or of Property * Property
+
+    let SumOf filter binop f = binop f (fun ts -> List.sumBy (sumByFilter filter))
+
+    type TransactionProperty = 
+        | And of TransactionProperty * TransactionProperty
+        | Or of TransactionProperty * TransactionProperty
         | SumOf of Filter * BinOp * float 
+        | CountOf of Filter * BinOp * int 
         | ForAllTimes of Property
+        | At of Time * Property
+    
+    ForAllTimes(SumOf())
 
     let rec evalProp property env (transactionsByDay as t) : bool = 
         match property with
-            | Predicate p -> p env t
             | And(p1,p2) -> evalProp p1 env t && evalProp p2 env t
             | Or(p1,p2) -> evalProp p1 env t || evalProp p2 env t
             | SumOf(filter,op,f) -> op f (Array.sumBy (fun ts -> List.sumBy (sumByFilter filter) ts) t)
+            | ForAllTimes(p) -> 
+               match Array.tryFind (fun tlist -> not (p env tlist)) t with
+               | Some(_) -> false 
+               | None -> true
+            | At(t,p) ->
+                let ts = transactionsByDay.[t]
+                p env ts
     
-    let (&&) p1 p2 : Property = And(p1,p2)
-    let (||) p1 p2 : Property = Or(p1,p2)
+    let (&&) p1 p2 : TransactionProperty = And(p1,p2)
+    let (||) p1 p2 : TransactionProperty = Or(p1,p2)
 
     let testProperty prop env ts = evalProp prop env ts
