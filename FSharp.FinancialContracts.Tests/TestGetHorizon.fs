b4a1d9153c94735226767c3ad3b36f7b1bfdbc6e
Frederik Reher <frederikreher@gmail.com>
2018-04-10 08:28:28 +0200

    Add test of gethorizon

---
+++ FSharp.FinancialContracts.Tests/TestGetHorizon.fs
@@ -0,0 +1,16 @@
+namespace FSharp.FinancialContracts.Tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FSharp.FinancialContracts.Contract
+
+[<TestClass>]
+type TestGetHorizon () =
+
+    [<TestMethod>]
+    member this.``Test that horizon is correct for simple delay`` () =
+        let contract = Delay(5,One DKK)
+              
+        let hor = getHorizon contract
+        
+        Assert.Equals(5,hor)
