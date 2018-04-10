570a60bf64c002541d0bebfd6cf79c77356b3b4c
Rasmus Rynning Rasmussen <r.r.rasmussen@hotmail.com>
2018-04-10 20:07:34 +0200

    added test for number of transactions in complex contract

--- FSharp.FinancialContracts.Tests/TestComplexContracts.fs
+++ FSharp.FinancialContracts.Tests/TestComplexContracts.fs
@@ -28,7 +28,7 @@ type TestComplexContracts () =
         Assert.AreEqual(331, horizon);
 
     [<TestMethod>]
-    member this.``Test sum of transactions of Complex Contract`` () = 
+    member this.``Test sum of transactions in Complex Contract`` () = 
         let Zcb = zcb 0 (Const 1.0) DKK
         let American = Delay(1, american (BoolVal "x") 3 (Scale(NumVal "y", Zcb)))
         let Asian = asian (BoolVal "x") (NumVal "y") 12 3 American
@@ -48,3 +48,28 @@ type TestComplexContracts () =
         let sumProperty = sumOf allTransactions (=) 1000.0
 
         PropertyCheck.CheckWithConfig config cc sumProperty
+
+    [<TestMethod>]
+    member this.``Test number of transactions in Complex Contract`` () = 
+        let ZcbDKK = zcb 0 (Const 1.0) DKK
+        let ZcbEUR = zcb 0 (Const 7.5) EUR
+        let am1 = american (BoolVal "x") 5 (Scale(NumVal "y", ZcbDKK))
+        let am2 = american (BoolVal "x") 3 (Scale(NumVal "z", ZcbEUR))
+        let American = Delay(1, And(am1, am2))
+        let Asian = asian (BoolVal "x") (NumVal "y") 12 3 American
+        let European = european (LessThan(NumVal "a", NumVal "b")) 18 Asian
+
+        let cc = European
+
+        let numGenMap = Map.empty
+                            .Add(NumVal "y", fun t -> if t = 33 then 50.0 else 20.0)
+                            .Add(NumVal "a", fun _ -> 5.0)
+                            .Add(NumVal "b", fun _ -> 10.0)
+        let boolGenMap = Map.empty
+                            .Add(BoolVal "x", fun t -> if (t = 30 || t = 33) then true else false)
+        
+        let config = {Configuration.Default with EnvironmentGenerator = EnvironmentGenerators.WithCustomGenerators numGenMap boolGenMap }
+
+        let amountProperty = countOf allTransactions (=) 2
+
+        PropertyCheck.CheckWithConfig config cc amountProperty
