7e61b325262e030c6f37ac213c934c3cd98f1a0c
Frederik Reher <frederikreher@gmail.com>
2018-05-09 14:27:22 +0200

    add gnerator tests

--- FSharp.FinancialContracts.Documentation/GeneratorPerformance.fs
+++ FSharp.FinancialContracts.Documentation/GeneratorPerformance.fs
@@ -11,22 +11,54 @@ open FSharp.FinancialContracts.Contracts
 open System
 
 module GeneratorPerformance = 
-    let Run =
-           let customGenerators = Map.empty
-                                           .Add( "x", BoolGenerators.BoolTrueAtTime 50)
-                                           .Add( "y", fun _ t -> if t = 20 then NumberValue 20.0 else NumberValue 10.0)
-                                           .Add( "z", fun _ _ -> NumberValue 20.0)
-                                                  
+    let Run () =
+//           let customGenerators = Map.empty
+//                                           .Add( "x", BoolGenerators.BoolTrueAtTime 50)
+//                                           .Add( "y", fun _ t -> if t = 20 then NumberValue 20.0 else NumberValue 10.0)
+//                                           .Add( "z", fun _ _ -> NumberValue 20.0)
+//                                                
+            let rec sum (numbobss:NumberObs list) = 
+                match numbobss with
+                   | [] -> Const 0.0
+                   | obs::rest -> Add(obs,sum rest)
                    
-                   
-           let generatorContract1 = If(BoolVal("x"),TimeObs.Const 365, One DKK, One EUR)
-           let generatorContract2 = If(BoolVal("x"),TimeObs.Const 365, Scale(NumVal("y"),One DKK), Scale(NumVal("z"),One DKK))
+            
+            let rec conjunction (boolobss:BoolObs list) = 
+                match boolobss with
+                   | [] -> Bool true
+                   | obs::rest -> BoolObs.And(obs,conjunction rest)
+            
+            let boolVals2 = [BoolVal "b1";BoolVal "b2"]
+            let boolVals4 = [BoolVal "b1";BoolVal "b2";BoolVal "b3";BoolVal "b4"]
+            let boolVals8 = [BoolVal "b1";BoolVal "b2";BoolVal "b3";BoolVal "b4";BoolVal "b5";BoolVal "b6";BoolVal "b7";BoolVal "b8"]
+            
+            let numVals2 = [NumVal "n1";NumVal "n2"]
+            let numVals4 = [NumVal "n1";NumVal "n2";NumVal "n3";NumVal "n4"]
+            let numVals8 = [NumVal "n1";NumVal "n2";NumVal "n3";NumVal "n4";NumVal "n5";NumVal "n6";NumVal "n7";NumVal "n8"]
+            
            
+            let generatorContract2Bool    = If(conjunction boolVals2, TimeObs.Const 365, One DKK, One EUR)
+            let generatorContract2Num     = If(Bool true, TimeObs.Const 365, Scale(sum numVals2, One DKK), One DKK)
+            let generatorContract2BoolNum = If(BoolVal("x"), TimeObs.Const 365, Scale(NumVal "y", One DKK), One EUR)
+            
+            let generatorContract4Bool    = If(conjunction boolVals4,TimeObs.Const 365, One DKK, One EUR)
+            let generatorContract4Num     = If(Bool true, TimeObs.Const 365, Scale(sum numVals4, One DKK), One DKK)
+            let generatorContract4BoolNum = If(conjunction boolVals2,TimeObs.Const 365, Scale(sum numVals2, One DKK), One EUR )
+            
+            let generatorContract8Bool    = If(conjunction boolVals8,TimeObs.Const 365, One DKK, One EUR)           
+            let generatorContract8Num     = If(Bool true, TimeObs.Const 365, Scale(sum numVals8, One DKK), One DKK)
+            let generatorContract8BoolNum = If(conjunction boolVals4,TimeObs.Const 365, Scale(sum numVals4, One DKK), One EUR )
+            
+            
+            let envGenerators = [
+                 (10000,generatorContract2Bool,EnvironmentGenerators.Default);
+                 (10000,generatorContract2Num,EnvironmentGenerators.Default);
+                 (10000,generatorContract2BoolNum,EnvironmentGenerators.Default);
+                 (10000,generatorContract4Bool,EnvironmentGenerators.Default);
+                 (10000,generatorContract4Num,EnvironmentGenerators.Default);
+                 (10000,generatorContract4BoolNum,EnvironmentGenerators.Default);
+                 (10000,generatorContract8Bool,EnvironmentGenerators.Default);
+                 (10000,generatorContract8Num,EnvironmentGenerators.Default); 
+                 (10000,generatorContract8BoolNum,EnvironmentGenerators.Default)]
            
-           let envGenerators = [(10000,generatorContract1,EnvironmentGenerators.Default);
-                                (10000,generatorContract1,EnvironmentGenerators.WithCustomGenerators customGenerators);
-                                (10000,generatorContract2,EnvironmentGenerators.Default)
-                                (10000,generatorContract2,EnvironmentGenerators.WithCustomGenerators customGenerators)
-                               ]
-           
-           PerformanceChecker.checkGeneratorPerformance envGenerators
+            PerformanceChecker.checkGeneratorPerformance envGenerators
--- FSharp.FinancialContracts.Documentation/Program.fs
+++ FSharp.FinancialContracts.Documentation/Program.fs
@@ -18,42 +18,9 @@ module program =
     [<EntryPoint>]
     let main argv =
         
-//        let work = fun i -> System.Threading.Thread.Sleep 1
-//                            i
-//        let doParallelWork n = Array.Parallel.init n work 
-//        
-//        let rec doSequentialWork (n:int) (i:int) =
-//            if n = i then
-//                ()
-//            else 
-//                let res = work i
-//                doSequentialWork n (i+1)
-//        
-//        let n = 10000
-//        let stopWatch = System.Diagnostics.Stopwatch.StartNew()
-//        doParallelWork n
-//        let t1 = stopWatch.Elapsed.TotalSeconds
-//        
-//        stopWatch.Restart()
-//        doSequentialWork n 0 
-//        let t2 = stopWatch.Elapsed.TotalSeconds
-//        
-//        printfn "Parallel was %A seconds sequential was %A seconds" t1 t2
-        
-        
-//        let contract = If(BoolObs.And(BoolVal "x",BoolVal "y"),TimeObs.Const 5, One DKK, One GBP)
-//        let property = atTime 1 (hasTransactions [Transaction(1.0, DKK)])
-//        
-//        let simpleConfiguration = {Configuration.Default with NumberOfTests = 10; FailSilently = false;}
-//        let testData = PropertyCheck.CheckAndReturnDataWithConfig simpleConfiguration contract property
-//        printfn "%A" testData
-//        printfn "%A" testData.TestsRun
-//        printfn "%A" testData.TestsFailed
-//        printfn "%A" testData.AccessLog
-            
         //EvaluationPerfomance.Run ()
-        //GeneratorPerformance.Run
-        PropertyCheckerPerformance.Run () 
+        GeneratorPerformance.Run ()
+        //PropertyCheckerPerformance.Run () 
             
         0 // return an integer exit code
     
